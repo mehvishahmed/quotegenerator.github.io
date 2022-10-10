660f9ab7d732355e81d68a336760fd6842be20c0
Mehvish Ahmed <84987764+mehvishahmed@users.noreply.github.com>
2022-10-10 13:43:55 -0600

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ generateQuote()
 // playing with colors
 function getRandomColor() {
     var letters = '0123456789ABCDEF';
-    var color = '#';
+    
     for (var i = 0; i < 6; i++) {
       color += letters[Math.floor(Math.random() * 16)];
     }
